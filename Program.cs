eed92929d48fc88619b5a05c7420406b4fdef75a
margsp <margsp@vivaldi.net>
2025-05-05 19:20:13 +0200

    Start, not even POC

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//POC:
+//get any files that are available in Input location (currently one)
+//apply some rules (in this case let's remove the left "aside" panel)
+//generate output mhtml
